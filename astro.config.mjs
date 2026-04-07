0f23360b0b14a3b74ac51cd94593632cfa349345
theonlyyff <smyfy1@outlook.com>
2026-04-08 01:16:11 +0800

    fix: strictly define node 20.x and update vercel adapter import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
+import vercel from '@astrojs/vercel';
 import clerk from '@clerk/astro';
 
 export default defineConfig({
